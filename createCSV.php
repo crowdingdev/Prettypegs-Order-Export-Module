d70b413fd2cd60ec8c6b3d9198633f3cefdfa9ff
crowdingdev <lundevallan@Linuss-MacBook-Pro.local>
2014-10-14 11:45:27 +0200

    fix

--- createCSV.php
+++ createCSV.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 /** Module Prettypegs orders export
   *file createCSV.php
   * @author Vinum Master, Linus Karlsson
@@ -136,55 +133,44 @@ function printRowToFile($order_detail){
 																		// 1 is the id_lang
 		addToFeed('"'.Country::getNameById(1,$order_detail['id_country'] ).'";');
 		addToFeed('"'.$country->iso_code.'";');
-		addToFeed('"'. ($order_detail['phone_mobile'] != '' ? $order_detail['phone_mobile'] : $order_detail['phone'] ).'";');
+		addToFeed('"'.($order_detail['phone_mobile'] != '' ? $order_detail['phone_mobile'] : $order_detail['phone'] ).'";');
 		addToFeed('"'.$order_detail['email'].'";');
 		addToFeed('"'.''.'";'); // payment left empty
 		addToFeed('"'.'NO'.'";');
 		addToFeed('"'.$reformatDate.'";');
+		addToFeed('"'.$order_detail['product_name'].'";');
 
 		addToFeed("\r\n");
 
 }
 
-
-// Astrid - Color : Yellow, Furniture & fittings : Universal Fitting Plate
-// Carl - Color : White, Furniture & fittings : M8, Size : Regular
-// Albin - FÃ¤rgval : Black, MÃ¶beltyp & beslag : M8
-// Albin - Farbe : Schwarz, MÃ¶bel & BeschlÃ¤ge : M8
-// Alfred Tischbein - Farbe : Schwarz, GrÃ¶ÃŸe : M
-// Albin - Farbe : Schwarz, MÃ¶bel & BeschlÃ¤ge : M8
-// Albin - FÃ¤rgval : Black, MÃ¶beltyp & beslag : M8
-// Hillevi - FÃ¤rgval : Ash natural, MÃ¶beltyp & beslag : M8
-// Estelle - FÃ¤rgval : Mint, MÃ¶beltyp & beslag : M8, Storlek : M
-// Svea - FÃ¤rgval : Ash / Mint, MÃ¶beltyp & beslag : M8
-// Carl bord - Färgval : Black, Storlek : M
-// Carl - Färgval : Teak style / blue, Möbeltyp & beslag : Universal Fitting Plate, Storlek : L
-
-
-
 function parseProductNameToProductCode($productName, $productAttributeId){
-	
-	$size = 'A';
+
+	$size = '';
 	$color = '';
 	$attributes = DBQueryHelper::getOrderDetailAttributeLang($productAttributeId);
 
 	foreach ($attributes as $attribute){
 
-		if ($attribute['attribute_name'] == 'Large' )
+		if ($attribute['attribute_name'] == 'Large')
 		{
-			$size = 'B';
+			$size = ' B';
 		}
-		if(strpos($productName,'Table')){
-			$size = 'T';
+		if ($attribute['attribute_name'] == 'Regular')
+		{
+			$size = ' A';
 		}
-
-		$all.= $attribute['attribute_name']. ', ';
+		if(strpos($productName,'Table'))
+		{
+			$size = ' T';
+		}
 
 		if (
 			$attribute['attribute_name'] != 'Large' &&
 			$attribute['attribute_name'] != 'Regular' &&
 			$attribute['attribute_name'] != 'M8' &&
 			$attribute['attribute_name'] != 'Universal Fitting Plate' &&
+			$attribute['attribute_name'] != 'One size' &&
 			$attribute['attribute_name'] != 'Fork Plate' ){
 
 			$color = $attribute['attribute_name'];
@@ -192,27 +178,16 @@ function parseProductNameToProductCode($productName, $productAttributeId){
 		}
 	}
 
-	$productName = str_replace ( ' Table' , '' , $productName);
-	return strtoupper($productName) . ' ' . $size . ' '. $color;
+	$color = str_replace ('style', '', $color);
+	$color = str_replace ('natural', '', $color);
+	$color = str_replace (' ', '', $color);
 
-	// $name = strtoupper (explode('-',$productName)[0]);
-	// $color = strtoupper (explode(':',$productName)[0]);
-	// preg_match('/:(.*?),/',$productName, $color);
-	// $color = str_replace(' ', '', $color[1]);
-	// $size = '';
-	// if (strpos($productName . ' ',' : M ' )){
-	// 	$size = 'A ';
-	// }
-	// if (strpos($productName . ' ',' : L ' )){
-	// 	$size = 'B ';
-	// }
-	// if (strpos( $productName,'table' ) || strpos($productName,'bord') || strpos($productName,'Tischbein')){
-	// 	$size = 'T ';
-	// }
-	// $name = str_replace ( 'BORD' , '' , $name);
-	// $name = str_replace ( 'TABLE' , '' , $name);
-	// $name = str_replace ( 'TISCHBEIN' , '' , $name);
-	// return rtrim ( $name) . ' ' . $size . $color   ;
+	$productName = str_replace ( ' Table' , '' , $productName);
+	$size = str_replace ( 'Onesize' , '' , $size);
+	$color = str_replace ( 'Onesize' , '' , $color);
+
+	return strtoupper($productName) . $size . ' '. $color;
+
 }
 
 function addToFeed($str)
